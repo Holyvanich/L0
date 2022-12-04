7a2209596a0320ed8ef66ee8029a6e01e970d87d
Ivan <iv151@mail.ru>
2022-12-05 00:48:38 +0300

    one more bugfix

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 
+
 function selectAll() {
     const chbox = document.getElementById('ok');
     const chbox1 = document.getElementById('ok1');
     const chbox2 = document.getElementById('ok2');
     const chbox3 = document.getElementById('ok3');
     if (chbox.checked) {
-        chbox1.checked = true;
-        chbox2.checked = true;
-        chbox3.checked = true;
+        if (chbox1) {
+            chbox1.checked = true;
+        }
+        if (chbox2) {
+            chbox2.checked = true;
+        }
+        if (chbox3) {
+            chbox3.checked = true;
+        }
     }
 
     else {
-        chbox1.checked = false;
-        chbox2.checked = false;
-        chbox3.checked = false;
+        if (chbox1) {
+            chbox1.checked = false;
+        }
+        if (chbox2) {
+            chbox2.checked = false;
+        }
+        if (chbox3) {
+            chbox3.checked = false;
+        }
     }
 }
 
@@ -214,6 +227,41 @@ function validateAll() {
     });
 }
 
+
+function submit() {
+    const email = document.getElementById('email');
+    email.addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        validateEmail(email.value, 1)
+      }
+    });
+    const phone = document.getElementById('phone');
+    phone.addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        validatePhone(phone.value, 1)
+      }
+    });
+    const name = document.getElementById('name');
+    name.addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        validateName(name.value, 'name', 1)
+      }
+    });
+    const surname = document.getElementById('surname');
+    surname.addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        validateName(surname.value, 'surname', 1)
+      }
+    });
+    const inn = document.getElementById('inn');
+    inn.addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        validateInn(inn.value, 1)
+      }
+    });
+};
+
+submit();
 
 
 
